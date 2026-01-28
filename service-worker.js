5f816f59fcbfdf1ee6ca17c767218b05d6a05fa5
marcinpietrzak23-create <marcin.pietrzak23@gmail.com>
2026-01-28 12:32:41 +0100

    service worker

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "hkec-api-v1";
+
+// które requesty cache’ujemy
+function isApiRequest(url) {
+  return (
+    url.includes("script.google.com/macros") &&
+    (
+      url.includes("arkusz=API_VIEW") ||
+      url.includes("arkusz=API_DODATKOWE") ||
+      url.includes("api=boje")
+    )
+  );
+}
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  const url = req.url;
+
+  if (!isApiRequest(url)) return;
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.match(req).then(cached => {
+
+        // fetch w tle (aktualizacja cache)
+        const fetchPromise = fetch(req)
+          .then(res => {
+            if (res && res.status === 200) {
+              cache.put(req, res.clone());
+            }
+            return res;
+          })
+          .catch(() => cached);
+
+        // cache-first
+        return cached || fetchPromise;
+      })
+    )
+  );
+});
